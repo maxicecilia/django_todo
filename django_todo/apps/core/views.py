fb48a559333aec75548d412a29fd1a0cbb4bdb5a
Maxi Cecilia <maxicecilia@gmail.com>
2014-12-21 03:18:57 -0300

    Adding template logic

--- django_todo/apps/core/views.py
+++ django_todo/apps/core/views.py
@@ -1,11 +1,14 @@
 import json
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
 from django_todo.apps.core.models import Task
 
 
 def current_tasks(request):
     tasks = Task.objects.filter(is_checked=False)
-    response = ''
-    for task in tasks:
-        response += task.description + '<br/>'
-    return HttpResponse(response)
+    template = loader.get_template('core/base.html')
+    context = RequestContext(request, {
+        'tasks': tasks,
+    })
+    return HttpResponse(template.render(context))
