ce46d2065a5f8f74a1c0cc93aa61cf1c5693e14d
Maxi Cecilia <maxicecilia@gmail.com>
2014-12-22 21:37:04 -0300

    Add new template logic

--- django_todo/apps/core/views.py
+++ django_todo/apps/core/views.py
@@ -7,7 +7,7 @@ from django_todo.apps.core.models import Task
 
 def current_tasks(request):
     tasks = Task.objects.filter(is_checked=False)
-    template = loader.get_template('core/base.html')
+    template = loader.get_template('core/current_tasks.html')
     context = RequestContext(request, {
         'tasks': tasks,
     })
