5f2252bc81aa647e938660cdd0834435538c2ea0
Maxi Cecilia <maxicecilia@gmail.com>
2014-12-31 13:43:14 -0300

    Refactor view to use django shortcuts

--- django_todo/apps/core/views.py
+++ django_todo/apps/core/views.py
@@ -1,14 +1,9 @@
 import json
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render_to_response
 
 from django_todo.apps.core.models import Task
 
 
 def current_tasks(request):
     tasks = Task.objects.filter(is_checked=False)
-    template = loader.get_template('core/current_tasks.html')
-    context = RequestContext(request, {
-        'tasks': tasks,
-    })
-    return HttpResponse(template.render(context))
+    return render_to_response('core/current_tasks.html', {'tasks': tasks, })
