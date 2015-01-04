71ed2de1ec77808b224c7a02fd6a0bc058bf4b7c
Maxi Cecilia <maxicecilia@gmail.com>
2015-01-04 18:44:48 -0300

    Add complete post using ajax.

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.decorators.csrf import csrf_exempt
 
 from django_todo.apps.core.views import CurrentTaskView, CompleteTaskView
 
@@ -7,7 +8,7 @@ urlpatterns = patterns(
     '',
     url(r'^$', CurrentTaskView.as_view(), name='home'),
     url(r'create/$', CurrentTaskView.as_view(), name='create_task'),
-    url(r'complete/(?P<id>\d+)/$', CompleteTaskView.as_view(), name='complete_task'),
+    url(r'complete/(?P<id>\d+)/$', csrf_exempt(CompleteTaskView.as_view()), name='complete_task'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
